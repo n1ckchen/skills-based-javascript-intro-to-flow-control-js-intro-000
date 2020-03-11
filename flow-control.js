7b27ab3f9ce331980764ab4db2c93ac1382cd729
nick chen <nick.chen.pro@gmail.com>
2020-03-11 16:52:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,7 +21,7 @@ function ageChecker(age) {
     return 'You are a teenager!';
   } else if (age <= 12) {
     return 'You are a kid';
-  } else {
+  } else if (age >= 20){
     return 'You are a grownup';
   }
 }
