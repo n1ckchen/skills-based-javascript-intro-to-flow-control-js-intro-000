29ba19b43613a2e9bbb001c5c443e1e93483149a
nick chen <nick.chen.pro@gmail.com>
2020-03-11 17:01:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,18 +31,18 @@ function ternaryTeenager(age) {
 
 }
 
- function switchAge(age) {
-  swtich (age) {
-  case 13:
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-  case 18:
-  case 19:
-    return 'You are a teenager';
-  default:
-    return 'You have an age';
-  }
-
-}
+//  function switchAge(age) {
+//   swtich (age) {
+//   case 13:
+//   case 14:
+//   case 15:
+//   case 16:
+//   case 17:
+//   case 18:
+//   case 19:
+//     return 'You are a teenager';
+//   default:
+//     return 'You have an age';
+//   }
+// 
+// }
